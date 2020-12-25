a0379f34164aa015827403e4ca35acb288ac155b
wdvxdr1123 <wdvxdr@foxmail.com>
2020-12-25 11:59:57 +0800

    fix regex

--- rules.go
+++ rules.go
@@ -91,7 +91,7 @@ func CommandRule(commands ...string) Rule {
 func RegexRule(regexPattern string) Rule {
 	regex := regexp.MustCompile(regexPattern)
 	return func(event *Event, state State) bool {
-		msg := event.Message.CQString()
+		msg := event.RawMessage
 		if regex.MatchString(msg) {
 			state["regex_matched"] = regex.FindStringSubmatch(msg)
 			return true
