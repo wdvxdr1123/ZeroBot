ee93a4b0f9552d66c9b5a427e9bd85e5f9a144cc
wdvxdr <wdvxdr@foxmail.com>
2021-02-14 16:19:49 +0800

    prevent dead lock

--- extension/manager/rule.go
+++ extension/manager/rule.go
@@ -1,3 +1,4 @@
+// Package manager provides a simple group plugin manager.
 package manager
 
 import (
@@ -16,12 +17,14 @@ var (
 	mu       = sync.RWMutex{}
 )
 
+// Manager is the interface of plugin group manager.
 type Manager interface {
 	zero.Hooker
 	Enable(groupID int64)
 	Disable(groupID int64)
 }
 
+// New returns Manager with settings.
 func New(service string, o *Options) Manager {
 	data, _ := bucket.Get([]byte(service))
 	m := &manager{
@@ -47,6 +50,7 @@ type manager struct {
 	states  map[int64]bool
 }
 
+// Enable enables a group to pass the manager.
 func (m *manager) Enable(groupID int64) {
 	m.Lock()
 	defer m.Unlock()
@@ -54,6 +58,7 @@ func (m *manager) Enable(groupID int64) {
 	_ = bucket.Put([]byte(m.service), pack(m.states))
 }
 
+// Disable disables a group to pass the manager.
 func (m *manager) Disable(groupID int64) {
 	m.Lock()
 	defer m.Unlock()
@@ -61,6 +66,7 @@ func (m *manager) Disable(groupID int64) {
 	_ = bucket.Put([]byte(m.service), pack(m.states))
 }
 
+// Hook impls the zero.Hooker, so you can use zero.AddHook to the file.
 func (m *manager) Hook() zero.Rule {
 	return func(event *zero.Event, state zero.State) bool {
 		m.RLock()
@@ -79,20 +85,32 @@ func (m *manager) Hook() zero.Rule {
 	}
 }
 
+// Lookup returns a Manager by the service name, if
+// not exist, it will returns nil.
 func Lookup(service string) Manager {
 	mu.RLock()
 	defer mu.RUnlock()
 	return managers[service]
 }
 
+// ForEach iterates through managers.
 func ForEach(iterator func(key string, manager Manager) bool) {
 	mu.RLock()
-	defer mu.RUnlock()
-	for k, v := range managers {
+	m := copyMap(managers)
+	mu.RUnlock()
+	for k, v := range m {
 		if !iterator(k, v) {
 			return
 		}
 	}
+}
+
+func copyMap(m map[string]Manager) map[string]Manager {
+	var ret = make(map[string]Manager, len(m))
+	for k, v := range m {
+		ret[k] = v
+	}
+	return ret
 }
 
 func pack(m map[int64]bool) []byte {
