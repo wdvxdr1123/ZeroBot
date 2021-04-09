a14cfe368f622d7936e4c07a64924373c2d1da97
wdvxdr <wdvxdr@foxmail.com>
2021-04-09 18:28:55 +0800

    fix(ext): single 多个反并发冲突

--- extension/single/single.go
+++ extension/single/single.go
@@ -6,15 +6,14 @@ import (
 	zero "github.com/wdvxdr1123/ZeroBot"
 )
 
-var group sync.Map
-
 // Option 配置项
 type Option func(*Single)
 
 // Single 反并发
 type Single struct {
-	key  func(ctx *zero.Ctx) interface{}
-	post func(ctx *zero.Ctx)
+	group sync.Map
+	key   func(ctx *zero.Ctx) interface{}
+	post  func(ctx *zero.Ctx)
 }
 
 // WithKeyFn 指定反并发的 Key
@@ -47,18 +46,18 @@ func (s *Single) Apply(engine *zero.Engine) {
 			return true
 		}
 		key := s.key(ctx)
-		if _, ok := group.Load(key); ok {
+		if _, ok := s.group.Load(key); ok {
 			if s.post != nil {
 				defer s.post(ctx)
 			}
 			return false
 		}
-		group.Store(key, struct{}{})
+		s.group.Store(key, struct{}{})
 		ctx.State["__single-key__"] = key
 		return true
 	})
 
 	engine.UsePostHandler(func(ctx *zero.Ctx) {
-		group.Delete(ctx.State["__single-key__"])
+		s.group.Delete(ctx.State["__single-key__"])
 	})
 }
