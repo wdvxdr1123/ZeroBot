01443f48c16915fd22d9820f841188e7ee76e676
wdvxdr <wdvxdr@foxmail.com>
2021-05-29 17:12:04 +0800

    refactor: remove command & add shell example
    写了一半，发现这个command有点复杂，还是选择以前的方式

--- example/command/main.go
+++ example/command/main.go
@@ -1,34 +1,30 @@
 package command
 
 import (
-	"github.com/wdvxdr1123/ZeroBot/command"
+	"flag"
+
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/extension/shell"
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
 func init() {
-	root := &command.Command{
-		Name: "test",
-		Run: func(ctx *command.Ctx) {
-			ctx.SendChain(
-				message.Text("test"),
-			)
-		},
-	}
-	command.AddRootCommand(root)
-
-	child1 := &command.Command{
-		Name: "test1",
-		Run: func(ctx *command.Ctx) {
-			ctx.SendChain(
-				message.Text("test1"),
-			)
-		},
-	}
-	root.AddCommand(child1)
-
-	// child2 := &command.Command{
-	// 	 Name: "test1",
-	//	 Run:  nil,
-	// }
-	// root.AddCommand(child2) // should panic: conflicted name
+	zero.OnCommand("github").Handle(func(ctx *zero.Ctx) {
+		fset := flag.FlagSet{}
+		var (
+			owner string
+			repo  string
+		)
+		fset.StringVar(&owner, "o", "wdvxdr1123", "")
+		fset.StringVar(&repo, "r", "ZeroBot", "")
+		arguments := shell.Parse(ctx.State["args"].(string))
+		err := fset.Parse(arguments)
+		if err != nil {
+			return
+		}
+		ctx.Send(message.Text("github\n" +
+			"owner: " + owner + "\n" +
+			"repo: " + repo,
+		))
+	})
 }
--- extension/shell/shell.go
+++ extension/shell/shell.go
@@ -1,4 +1,4 @@
-package command
+package shell
 
 import "strings"
 
