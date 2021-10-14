cc04aa23ad612b351f26fc1e437a2dd2e0e59a6c
wdvxdr <wdvxdr1123@gmail.com>
2021-10-14 20:07:11 +0800

    fix: vet failed on StringToBytes

--- utils/helper/helper.go
+++ utils/helper/helper.go
@@ -13,7 +13,7 @@ func BytesToString(b []byte) string {
 // StringToBytes 没有内存开销的转换
 func StringToBytes(s string) (b []byte) {
 	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	sh := *(*reflect.StringHeader)(unsafe.Pointer(&s)) // nolint
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	bh.Data = sh.Data
 	bh.Len = sh.Len
 	bh.Cap = sh.Len
