6a57540f4b3f26040e5d104fd0924e2adee956be
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-10-29 20:04:42 +0800

    feat: 无锁的事件环

--- ring.go
+++ ring.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+	"unsafe"
 )
 
 type eventRing struct {
@@ -15,8 +16,6 @@ type eventRing struct {
 }
 
 type eventRingItem struct {
-	sync.Mutex
-	recvtime int64 // atomic field
 	response []byte
 	caller   APICaller
 }
@@ -27,7 +26,6 @@ func newring(ringLen uint) eventRing {
 	// Initialize the ring with locked eventRing
 	for i := 0; i < n; i++ {
 		evr := &eventRingItem{}
-		evr.Lock()
 		r.Value = evr
 		r = r.Next()
 	}
@@ -39,13 +37,12 @@ func (evr *eventRing) processEvent(response []byte, caller APICaller) {
 	evr.Lock()
 	defer evr.Unlock()
 	r := evr.r
-	it := r.Value.(*eventRingItem)
-	if !atomic.CompareAndSwapInt64(&it.recvtime, 0, time.Now().UnixNano()) { // 池满, 丢弃事件
-		return
-	}
-	it.response = response
-	it.caller = caller
-	it.Unlock() // 开始处理事件
+	atomic.StorePointer((*unsafe.Pointer)(unsafe.Add(unsafe.Pointer(&r.Value), unsafe.Sizeof(uintptr(0)))),
+		unsafe.Pointer(&eventRingItem{
+			response: response,
+			caller:   caller,
+		}),
+	)
 	evr.r = r.Next()
 }
 
@@ -54,16 +51,16 @@ func (evr *eventRing) processEvent(response []byte, caller APICaller) {
 //	latency 延迟 latency + (0~100ms) 再处理事件
 func (evr *eventRing) handle(latency, maxwait time.Duration) {
 	r := evr.r
-	for {
+	for range time.NewTicker(latency).C {
 		it := r.Value.(*eventRingItem)
-		it.Lock()
-		if time.Now().UnixNano()-it.recvtime < int64(maxwait/time.Nanosecond) {
-			time.Sleep(latency + time.Duration(rand.Intn(100))*time.Millisecond)
-			processEventAsync(it.response, it.caller, maxwait)
-		} // 等待时间太长，不做处理直接跳过
+		if it == nil { // 还未有消息
+			continue
+		}
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		processEventAsync(it.response, it.caller, maxwait)
 		it.response = nil
 		it.caller = nil
-		atomic.StoreInt64(&it.recvtime, 0)
+		atomic.StorePointer((*unsafe.Pointer)(unsafe.Add(unsafe.Pointer(&r.Value), unsafe.Sizeof(uintptr(0)))), unsafe.Pointer(nil))
 		r = r.Next()
 		runtime.GC()
 	}
--- ring_test.go
+++ ring_test.go
@@ -2,28 +2,30 @@ package zero
 
 import (
 	"fmt"
+	"math/rand"
 	"runtime"
 	"sync/atomic"
 	"testing"
 	"time"
+	"unsafe"
 )
 
 var buf [256]byte
 
 func TestRing(t *testing.T) {
 	r := newring(128)
-	go r.testHandle(time.Millisecond, time.Second)
+	go r.testHandle(8 * time.Millisecond)
 	for i := 0; i < 256; i++ {
 		r.testProcessEvent([]byte{byte(i), byte(i)}, nil)
 	}
-	time.Sleep(time.Second)
-	for i := 0; i < 128; i++ {
+	time.Sleep(8 * time.Millisecond * 300)
+	for i := 128; i < 256; i++ {
 		if buf[i] != byte(i) {
 			t.Fatal("ring missed", i)
 		}
 		buf[i] = 0
 	}
-	for i := 128; i < 256; i++ {
+	for i := 0; i < 128; i++ {
 		if buf[i] != 0 {
 			t.Fatal("unexpected ring value at", i)
 		}
@@ -44,37 +46,32 @@ func (evr *eventRing) testProcessEvent(response []byte, caller APICaller) {
 	evr.Lock()
 	defer evr.Unlock()
 	r := evr.r
-	it := r.Value.(*eventRingItem)
-	if !atomic.CompareAndSwapInt64(&it.recvtime, 0, time.Now().UnixNano()) { // 池满, 丢弃事件
-		fmt.Println("drop", response[0])
-		return
-	}
-	it.response = response
-	it.caller = caller
+	atomic.StorePointer((*unsafe.Pointer)(unsafe.Add(unsafe.Pointer(&r.Value), unsafe.Sizeof(uintptr(0)))),
+		unsafe.Pointer(&eventRingItem{
+			response: response,
+			caller:   caller,
+		}),
+	)
 	fmt.Println("fill", response[0])
-	it.Unlock() // 开始处理事件
 	evr.r = r.Next()
 }
 
 // handle 循环处理事件
 //
 //	latency 延迟 latency + (0~1000ms) 再处理事件
-func (evr *eventRing) testHandle(latency time.Duration, maxwait time.Duration) {
+func (evr *eventRing) testHandle(latency time.Duration) {
 	r := evr.r
-	for {
+	for range time.NewTicker(latency).C {
 		it := r.Value.(*eventRingItem)
-		it.Lock()
-		fmt.Println("no:", it.response[0], "diff:", time.Now().UnixNano()-it.recvtime, "max:", int64(maxwait/time.Nanosecond))
-		if time.Now().UnixNano()-it.recvtime < int64(maxwait/time.Nanosecond) {
-			time.Sleep(latency)
-			buf[it.response[0]] = it.response[1]
-			fmt.Println(it.response[0], "processed")
-		} else {
-			fmt.Println("skip", it.response[0])
-		} // 等待时间太长，不做处理直接跳过
+		if it == nil { // 还未有消息
+			continue
+		}
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Microsecond)
+		buf[it.response[0]] = it.response[1]
+		fmt.Println(it.response[0], "processed")
 		it.response = nil
 		it.caller = nil
-		atomic.StoreInt64(&it.recvtime, 0)
+		atomic.StorePointer((*unsafe.Pointer)(unsafe.Add(unsafe.Pointer(&r.Value), unsafe.Sizeof(uintptr(0)))), unsafe.Pointer(nil))
 		r = r.Next()
 		runtime.GC()
 	}
